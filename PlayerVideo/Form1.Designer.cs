c2b611cc57796e5a1e5df36b67eccad76937438d
jael_ <metzguer@gmx.es>
2021-06-29 18:13:43 -0500

    Add project files.

---
+++ PlayerVideo/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace PlayerVideo
+{
+    partial class Player
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Player));
+            this.panelMedia = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.searchFiles = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.trVol = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelMedia.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trVol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMedia
+            // 
+            this.panelMedia.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMedia.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.panelMedia.Controls.Add(this.panel3);
+            this.panelMedia.Controls.Add(this.panel1);
+            this.panelMedia.Location = new System.Drawing.Point(0, 0);
+            this.panelMedia.Name = "panelMedia";
+            this.panelMedia.Size = new System.Drawing.Size(987, 500);
+            this.panelMedia.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.trVol);
+            this.panel2.Controls.Add(this.btnNext);
+            this.panel2.Controls.Add(this.btnPrev);
+            this.panel2.Controls.Add(this.btnStop);
+            this.panel2.Controls.Add(this.btnPause);
+            this.panel2.Controls.Add(this.btnPlay);
+            this.panel2.Controls.Add(this.btnLoad);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 497);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(987, 73);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnLoad.Location = new System.Drawing.Point(12, 22);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(89, 36);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Cargar";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnPlay.Location = new System.Drawing.Point(132, 22);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(89, 36);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPause.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnPause.Location = new System.Drawing.Point(244, 22);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(89, 36);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnStop.Location = new System.Drawing.Point(349, 22);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(89, 36);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // searchFiles
+            // 
+            this.searchFiles.FileName = "openFileDialog1";
+            this.searchFiles.Filter = "Todos|*.*";
+            this.searchFiles.Multiselect = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(748, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(239, 488);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.mediaPlayer);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(748, 491);
+            this.panel3.TabIndex = 1;
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(0, 0);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(748, 491);
+            this.mediaPlayer.TabIndex = 0;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrev.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnPrev.Location = new System.Drawing.Point(449, 22);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(89, 36);
+            this.btnPrev.TabIndex = 6;
+            this.btnPrev.Text = "Prev";
+            this.btnPrev.UseVisualStyleBackColor = false;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnNext.Location = new System.Drawing.Point(544, 22);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(89, 36);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // trVol
+            // 
+            this.trVol.Location = new System.Drawing.Point(731, 17);
+            this.trVol.Name = "trVol";
+            this.trVol.Size = new System.Drawing.Size(244, 56);
+            this.trVol.TabIndex = 8;
+            this.trVol.ValueChanged += new System.EventHandler(this.trVol_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(682, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Vol.";
+            // 
+            // Player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 570);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelMedia);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Player";
+            this.Text = "Player media";
+            this.panelMedia.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trVol)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMedia;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.OpenFileDialog searchFiles;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.TrackBar trVol;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PlayerVideo/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace PlayerVideo
+{
+    public partial class Player : Form
+    {
+        private static string path = string.Empty;
+        private static string[] files;
+        public Player()
+        {
+            InitializeComponent();
+            mediaPlayer.uiMode = "none";
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (searchFiles.ShowDialog() == DialogResult.OK) {
+
+                int filesSelected = searchFiles.FileNames.Length;
+                if (filesSelected <= 0)
+                {
+                    MessageBox.Show("Error en la lectura de los archivos");
+                } 
+                else { 
+                    files = new string[filesSelected];
+
+                    IWMPPlaylist playlst = mediaPlayer.playlistCollection.newPlaylist("test");
+
+                    files = searchFiles.FileNames;
+
+                    foreach (string file in files) {
+                        path = file;
+                        IWMPMedia media = mediaPlayer.newMedia(path);
+
+                        playlst.appendItem(media);
+
+                    }
+                    IWMPMedia media2 = mediaPlayer.newMedia(path);
+                    mediaPlayer.settings.setMode("loop", true);
+                    mediaPlayer.currentPlaylist = playlst;
+                    mediaPlayer.Ctlcontrols.play(); 
+                    mediaPlayer.settings.volume = trVol.Value;
+                }
+            }
+        
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.URL = path;
+            mediaPlayer.Ctlcontrols.play();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.stop();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.previous();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.next();
+        }
+
+        private void trVol_ValueChanged(object sender, EventArgs e)
+        {
+            int volMedia = trVol.Value;
+            mediaPlayer.settings.volume = volMedia;
+        }
+
+        private void SelectPlayItem() {
+
+            mediaPlayer.Ctlcontrols.playItem( mediaPlayer.currentPlaylist.Item[1] );
+        }
+    }
+}
